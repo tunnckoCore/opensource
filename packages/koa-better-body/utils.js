ee084072d9385d7ecea704c3bbc3ec10ca06a800
tunnckoCore <mameto2011@gmail.com>
2017-07-16 16:27:49 +0300

    fix(lint): update linting, remove lazy-cache
    use eslint for lint, install both eslint and standard intentionally

--- packages/koa-better-body/utils.js
+++ packages/koa-better-body/utils.js
@@ -4,36 +4,11 @@
  * Module dependencies
  */
 
-var utils = require('lazy-cache')(require)
-
-/**
- * Temporarily re-assign `require` to trick browserify and
- * webpack into reconizing lazy dependencies.
- *
- * This tiny bit of ugliness has the huge dual advantage of
- * only loading modules that are actually called at some
- * point in the lifecycle of the application, whilst also
- * allowing browserify and webpack to find modules that
- * are depended on but never actually called.
- */
-
-var fn = require
-require = utils // eslint-disable-line no-undef, no-native-reassign, no-global-assign
-
-/**
- * Lazily required module dependencies
- */
-
-require('extend-shallow', 'extend')
-require('formidable')
-require('koa-body-parsers', 'bodyParsers')
-require('querystring')
-
-/**
- * Restore `require`
- */
-
-require = fn // eslint-disable-line no-undef, no-native-reassign, no-global-assign
+var utils = {}
+utils.extend = require('extend-shallow')
+utils.formidable = require('formidable')
+utils.querystring = require('querystring')
+utils.bodyParsers = require('koa-body-parsers')
 
 /**
  * > Default options that will be loaded. Pass `options` to overwrite them.
