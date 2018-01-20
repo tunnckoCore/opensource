928c853737dcd9af3ffdc18c874362343f087c6d
Łukasz Gandecki <lgandecki@css.edu>
2018-01-20 16:56:09 +0400

    wrong test title

--- @tunnckocore/create-jest-runner/integrationTests/skipped.test.js
+++ @tunnckocore/create-jest-runner/integrationTests/skipped.test.js
@@ -1,5 +1,5 @@
 const runJest = require('./runJest');
 
-it('Works when it has failing tests', () => {
+it('Works when it has skipped tests', () => {
   return expect(runJest('skipped')).resolves.toMatchSnapshot();
 });
