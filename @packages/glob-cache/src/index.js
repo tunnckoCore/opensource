a77e7f7a1bd1478500cad9ae36dd5efda528a47a
Charlike Mike Reagent <ceo@tunnckocore.com>
2020-03-05 04:04:01 +0200

    feat(glob-cache): streaming API, use async iterables under the h… (#124)
    * feat: streaming API, use async iterables under the hood
    * chore: docs + glob(patterns, options) mirror of glob.stream(options)
    
    BREAKING CHANGE: use async iterables + expose Stream, Promise, and Hooks APIs
    
    ## "Streaming" / async iterables API
    Uses `fastGlob.stream` by default, plus `for await ... of`.
    
    The main default export is async generator `async function * () {}` and it returns async iterable, so you need to call `iterable.next()` or to use `for await` loop (which requires `async` function).
    
    ```js
    const globCache = require('glob-cache');
    
    async function main() {
      const iterable = globCache('src/**/*.js');
      // or like so, both are equivalent
      const iter = globCache.stream({ include: 'src/**/*.js' });
    
      for await (const ctx of iterable) {
        console.log(ctx);
      }
    }
    
    main().catch(console.error);
    ```
    
    ## Promise API
    
    There is also exported `globCache.promise` API, which is `async function` and so returns a Promise.
    
    ```js
    const globCache = require('glob-cache');
    
    const promise = globCache.promise({ include: 'src/**/*.js' });
    
    promise
      .then((results) => {
        console.log(results); // []
      })
      .catch(console.error);
    ```
    
    Important to note. By default the Promise API resolves to an empty array. That's intentional since we also have the so-called Hooks API and so it's unnecessary to pollute the memory when you are using this api. If you don't use the Hooks API, but just want the results then pass `buffered: true` and the promise will resolve to an array of Context objects. You can later filter this `results` array of contexts by `ctx.changed` or `ctx.notFound`, or whatever.
    
    ```js
    const globCache = require('glob-cache');
    
    async function main() {
      const results = await globCache.promise({
        include: 'src/**/*.js',
        buffered: true,
      });
    
      console.log(results);
      // => [Context, Context, Context, ...]
    }
    
    main().catch(console.error);
    ```
    
    ## Hooks API
    
    **It's not recommended to use the Hooks API when using the Stream API.**
    
    Previously we had just a single `options.hook` function, now it is `options.hooks` object. And we needed to determine based on `ctx.valid` and `ctx.missing` booleans. 
    
    Now we just have hooks for everything - `hooks.changed`, `hooks.notChanged`, `hooks.found`, `hooks.notFound` and `hooks.always`. It's pretty obvious by their names. Hooks can also be async functions - using async/await or regular function returning a promise.
    
    The `ctx.valid` and `ctx.missing` are replaced by `ctx.changed` and `ctx.notFound` - both has almost the same meaning as previously. Both are available through all the hooks. In combination with hooks it becomes great.
    
    ```
    1. on very first hit
      -> ctx.changed === true
      -> ctx.notFound === true
    2. on second hit (without changes to files)
      -> ctx.changed === false
      -> ctx.notFound === false
    3. on third hit (with changes)
      -> ctx.changed === true
      -> ctx.notFound === false
    ```
    
    Same as above applies for the hooks calls.
    
    ```js
    const globCache = require('glob-cache');
    
    (async () => {
    
    await globCache.promise({
      include: 'src/*.js',
      hooks: {
        changed(ctx) {
          if (ctx.notFound) {
            console.log(ctx.file.path, 'first hit');
          } else {
            console.log(ctx.file.path, 'changed');
          }
        },
        always() {
          console.log('file', ctx.file.path);
        },
      },
    });
    
    })()
    ```
    
    Notice that `changed` hook is also called when "not found" (i.e. first hit).
    
    Signed-off-by: Charlike Mike Reagent <opensource@tunnckocore.com>

--- @packages/glob-cache/src/index.js
+++ @packages/glob-cache/src/index.js
@@ -1,191 +1,258 @@
-/* eslint-disable no-param-reassign */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable max-statements */
 
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 const util = require('util');
 const crypto = require('crypto');
-const cacache = require('cacache');
 const fastGlob = require('fast-glob');
-const arrayDiff = require('arr-diff');
+const cacache = require('cacache');
+const memoizeFs = require('memoize-fs');
 
 const readFile = util.promisify(fs.readFile);
-const defaultOptions = {
-  include: [],
-  exclude: ['**/node_modules'],
-  hook: () => {},
-  always: false,
-  glob: fastGlob,
-  globOptions: {},
-  cacheLocation: './.cache/glob-cache',
-};
 
 /**
- * Match files and folders using glob patterns. Returns a resolved Promise containing
- * a `{ results, cacache }` object - where `results` is an array of [Context](#context-and-how-it-works) objects
- * and `cacache` is the [cacache][] package.
+ * A mirror of `globCache.stream` and so an "async generator" function,
+ * returning an AsyncIterable. This mirror exists because it's
+ * a common practice to have a `(globPatterns, options)` signature.
  *
  * @example
- * const tinyGlob = require('tiny-glob');
- * const glob = require('glob-cache');
+ * const globCache = require('glob-cache');
  *
- * glob({ include: 'src/*.js', glob: tinyGlob }).then(({ results }) => {
- *   console.log(results);
+ * const iterable = globCache(['src/*.js', 'test/*.{js,ts}'], {
+ *   cwd: './foo/bar'
  * });
  *
+ * // equivalent to
+ *
+ * const iter = globCache.stream({
+ *   include: ['src/*.js', 'test/*.{js,ts}'],
+ *   cwd: './foo/bar'
+ * });
  *
  * @name  globCache
- * @param {string|Array<string>} options.include - string or array of string glob patterns
- * @param {string} options.exclude - ignore patterns
- * @param {boolean} options.always - a boolean that makes `options.hook` to always be called
- * @param {Function} options.hook - a hook function passed with [Context](#context-and-how-it-works)
- * @param {Function} options.glob - a globbing library like [glob][], [globby][], [fast-glob][], [tiny-glob][], defaults to `fast-glob`
- * @param {object} options.globOptions - options passed to the `options.glob` library
- * @param {string} options.cacheLocation - a filepath location of the cache, defaults to `./.cache/glob-cache`
- * @returns {Promise}
+ * @param {string|Array} patterns - string or array of glob patterns
+ * @param {object} options - see `globCache.stream` options
  * @public
  */
-module.exports = async function globCache(options) {
-  const opts = { ...defaultOptions, ...options };
-
-  /* istanbul ignore next */
-  if (typeof opts.glob !== 'function') {
-    opts.glob = fastGlob;
-  }
-
-  const files = await Promise.all(
-    (
-      await opts.glob(opts.include, {
-        ...options.globOptions,
-        unique: true,
-        absolute: true,
-        objectMode: true,
-        ignore: opts.exclude,
-      })
-    ).map(async (file) => {
-      if (typeof file === 'string') {
-        file = { path: file };
-      }
-      file.contents = await readFile(file.path);
-      return file;
-    }),
-  );
-
-  /* istanbul ignore next */
-  const hook = typeof opts.hook !== 'function' ? () => {} : opts.hook;
-  const cacheLoc = opts.cacheLocation;
-  const cached = await cacache.ls(cacheLoc);
-  const cacheFiles = Object.keys(cached);
-  const results = [];
-
-  const missingFilesInCache = arrayDiff(
-    files.map((x) => x.path),
-    cacheFiles,
-  );
-
-  // source files are missing in cache, add them
-  if (missingFilesInCache.length > 0) {
-    // console.log('has missing', missingFilesInCache);
-    await Promise.all(
-      missingFilesInCache
-        .reduce(
-          (acc, fp) =>
-            acc.concat(files.find((x) => x.path === fp)).filter(Boolean),
-          [],
-        )
-        .map(async ({ path: filepath, contents }) => {
-          const res = await cacache.put(
-            cacheLoc,
-            filepath,
-            contents.toString(),
-          );
-
-          const ctx = {
-            file: {
-              path: filepath,
-              contents,
-              size: contents.length,
-              integrity: res.sha512[0].source,
-            },
-            cacheFile: null,
-            cacheLocation: cacheLoc,
-            cacache,
-            valid: true,
-            missing: true,
-          };
-
-          results.push(ctx);
-          if (opts.always === true) {
-            await hook(ctx);
-          }
-        }),
-    );
-    // console.log('they are written to cache');
-  }
-
-  if (cacheFiles.length > 0) {
-    // console.log('cacheFiles');
-    await Promise.all(
-      cacheFiles.map(async (fp) => {
-        const cacheFile = cached[fp];
-        // most probably the source file was deleted
-        /* istanbul ignore next */
-        if (!fs.existsSync(fp)) {
-          // console.log('delete from cache:', fp);
-
-          // ? should we delete the index too (doesn't seem to work anyway)?
-          await cacache.rm.entry(cacheLoc, fp);
-          await cacache.rm.content(cacheLoc, cacheFile.integrity);
-          await cacache.verify(cacheLoc);
-          return;
-        }
-        const contents = await readFile(fp);
-        const integrity = integrityFromContents(contents);
-        const hash = await cacache.get.hasContent(cacheLoc, integrity);
-
-        const valid = hash !== false;
-        const file = { path: fp, contents, size: contents.length, integrity };
-        const ctx = {
-          file,
-          cacheFile: hash
-            ? { ...cacheFile, sri: hash.sri, stat: hash.stat }
-            : cacheFile,
-          cacheLocation: cacheLoc,
-          cacache,
-          valid,
-          missing: false,
-        };
-
-        results.push(ctx);
-
-        if (opts.always === true || valid === false) {
-          await hook(ctx);
-        }
-      }),
-    );
-  }
-
-  return { results, cacache };
-};
-
-function integrityFromContents(contents, hash = 'sha512') {
-  const integrity = crypto
-    .createHash(hash)
-    .update(contents)
-    .digest('base64');
-
-  return `${hash}-${integrity}`;
+async function* globCache(patterns, options) {
+  yield* globCache.stream({ ...options, patterns });
 }
 
-// async function hook(context) {
-//   console.log('ctx', context);
-//   // updateCache(context);;;;;;;;;
-// }
+/**
+ * Match files and folders with glob patterns, by default using [fast-glob's `.stream()`](https://ghub.now.sh/fast-glob).
+ * This function is [async generator](https://javascript.info/async-iterators-generators)
+ * and returns "async iterable", so you can use the `for await ... of` loop. Note that this loop
+ * should be used inside an `async function`.
+ * Each item is a [Context](#context-and-how-it-works) object, which is also passed to each hook.
+ *
+ * @example
+ * const globCache = require('glob-cache');
+ *
+ * (async () => {
+ *   // Using the Stream API
+ *   const iterable = globCache.stream({
+ *     include: 'src/*.js',
+ *     cacheLocation: './foo-cache'
+ *   });
+ *
+ *   for await (const ctx of iterable) {
+ *     console.log(ctx);
+ *   }
+ * })();
+ *
+ * @name  globCache.stream
+ * @param {string} options.cwd - working directory, defaults to `process.cwd()`
+ * @param {string|Array} options.include - string or array of string glob patterns
+ * @param {string|Array} options.patterns - alias of `options.include`
+ * @param {string|Array} options.exclude - ignore glob patterns, passed to `options.globOptions.ignore`
+ * @param {string|Array} options.ignore - alias of `options.exclude`
+ * @param {object} options.hooks - an object with hooks functions, each hook passed with [Context](#context-and-how-it-works)
+ * @param {Function} options.hooks.found - called when a cache for a file is found
+ * @param {Function} options.hooks.notFound - called when file is not found in cache (usually the first hit)
+ * @param {Function} options.hooks.changed - called always when source file differs the cache file
+ * @param {Function} options.hooks.notChanged - called when both source file and cache file are "the same"
+ * @param {Function} options.hooks.always - called always, no matter of the state
+ * @param {Function} options.glob - a function `(patterns, options) => {}` or globbing library like [glob][], [globby][], [fast-glob][]
+ * @param {object} options.globOptions - options passed to the `options.glob` library
+ * @param {string} options.cacheLocation - a filepath location of the cache, defaults to `.cache/glob-cache` in `options.cwd`
+ * @returns {AsyncIterable}
+ * @public
+ */
+globCache.stream = async function* globCacheStream(options) {
+  const settings = { ...options };
+  const opts = {
+    buffered: false,
+    cwd: process.cwd(),
+    exclude: ['**/node_modules/**'],
+    ...settings,
+    hooks: {
+      always() {},
+      changed() {},
+      notChanged() {},
+      found() {},
+      ...settings.hooks,
+    },
+  };
 
-// main({
-//   include: ['**/*.js', '*.json'],
-//   // hook,
-// }).then(({ results }) => {
-//   // console.log(files);
-//   console.log('Done!');
-// });
+  opts.cacheLocation =
+    opts.cacheLocation && typeof opts.cacheLocation === 'string'
+      ? opts.cacheLocation
+      : path.join(opts.cwd, '.cache', 'glob-cache');
+
+  const { glob = fastGlob.stream, globOptions, hooks } = opts;
+
+  Object.keys(hooks).forEach((name) => {
+    const hook = hooks[name];
+    if (typeof hook !== 'function') {
+      throw new TypeError(`glob-cache: expect hook "${name}" to be function`);
+    }
+  });
+
+  hooks.notFound =
+    hooks.notFound ||
+    (async ({ file }) => {
+      await cacache.put(opts.cacheLocation, file.path, file.contents);
+    });
+
+  const memoizer = memoizeFs({
+    cachePath: path.join(opts.cwd, '.cache', 'glob-meta-cache'),
+  });
+
+  const globConfig = {
+    ignore: arrayify(opts.ignore || opts.ignores || opts.exclude),
+    cwd: opts.cwd,
+    ...globOptions,
+    unique: true,
+    absolute: true,
+    objectMode: true,
+  };
+
+  const iterable = await glob(opts.patterns || opts.include, globConfig);
+  const integrityMemoized = await memoizer.fn(getIntegrityFor);
+
+  for await (let data of iterable) {
+    // in case of `globby()` or promisified `node-glob`
+    data =
+      typeof data === 'string'
+        ? { path: data, name: path.basename(data) }
+        : data;
+
+    const contents = await readFile(data.path);
+    const integrity = await integrityMemoized(contents);
+    const info = await cacache.get.info(opts.cacheLocation, data.path);
+    const hash = await cacache.get.hasContent(opts.cacheLocation, integrity);
+
+    const file = {
+      ...data,
+      contents,
+      size: contents.length,
+      integrity,
+    };
+
+    const ctx = {
+      file,
+      changed: hash === false,
+      notFound: info === null,
+      cacache,
+      cacheLocation: opts.cacheLocation,
+    };
+
+    ctx.cacheFile = info;
+    if (hash) {
+      ctx.cacheFile.stat = hash.stat;
+    }
+
+    if (ctx.changed) {
+      if (ctx.notFound) {
+        await hooks.notFound(ctx, opts);
+      } else {
+        await hooks.found(ctx, opts);
+      }
+      await hooks.changed(ctx, opts);
+    } else {
+      await hooks.found(ctx, opts);
+      await hooks.notChanged(ctx, opts);
+    }
+    await hooks.always(ctx, opts);
+    yield ctx;
+  }
+};
+
+function hasha(value, opts) {
+  const { algorithm, digest } = { ...opts };
+  return crypto
+    .createHash(algorithm)
+    .update(value)
+    .digest(digest);
+}
+
+function getIntegrityFor(contents, hash = 'sha512') {
+  const id = hasha(contents, { algorithm: hash, digest: 'base64' });
+
+  return `${hash}-${id}`;
+}
+
+function arrayify(val) {
+  return [].concat(val).filter(Boolean);
+}
+
+/**
+ * Using the Promise API allows you to use the Hooks API, and it's actually
+ * the recommended way of using the hooks api. By default, if the returned promise
+ * resolves, it will be an empty array. That's intentional, because if you are
+ * using the hooks api it's unnecessary to pollute the memory putting huge objects
+ * to a "result array". So if you want results array to contain the Context objects
+ * you can pass `buffered: true` option.
+ *
+ * @example
+ * const globCache = require('glob-cache');
+ * const globby = require('globby');
+ *
+ * (async () => {
+ *   // Using the Hooks API and `globby.stream`
+ *   const res = await globCache.promise({
+ *     include: 'src/*.js',
+ *     cacheLocation: './.cache/awesome-cache',
+ *     glob: globby.stream,
+ *     hooks: {
+ *       changed(ctx) {},
+ *       always(ctx) {},
+ *     }
+ *   });
+ *   console.log(res); // => []
+ *
+ *   // Using the Promise API
+ *   const results = await globCache.promise({
+ *     include: 'src/*.js',
+ *     exclude: 'src/bar.js',
+ *     buffered: true,
+ *   });
+ *
+ *   console.log(results); // => [Context, Context, ...]
+ * })();
+ *
+ * @name  globCache.promise
+ * @param {object} options - see `globCache.stream` options, in addition here we have `options.buffered` too
+ * @param {boolean} options.buffered - if `true` returned array will contain [Context]((#context-and-how-it-works)) objects, default `false`
+ * @returns {Promise} if `options.buffered: true` resolves to `Array<Context>`, otherwise empty array
+ * @public
+ */
+globCache.promise = async function globCachePromise(options) {
+  const stream = globCache.stream(options);
+  const results = [];
+
+  for await (const ctx of stream) {
+    // do not put on memory if not necessary,
+    // because we may want to just use the Hooks API
+    if (options.buffered) {
+      results.push(ctx);
+    }
+  }
+
+  return results;
+};
+
+module.exports = globCache;
