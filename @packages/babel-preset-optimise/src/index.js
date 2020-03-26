5ea0b1a3f38df1707337462f1c1e6175a4fdfd90
Charlike Mike Reagent <opensource@tunnckocore.com>
2020-03-26 15:31:48 +0200

    chore: add options.development, updates, regen docs
    Signed-off-by: Charlike Mike Reagent <opensource@tunnckocore.com>

--- @packages/babel-preset-optimise/src/index.js
+++ @packages/babel-preset-optimise/src/index.js
@@ -4,10 +4,10 @@
  * Be aware that when you use `minifyBuiltins: true` you _MAY_ get a bigger output,
  * but that's not always guaranteed, just try for your case.
  *
- * If you want to use JSX (React) in typescript, pass `options.jsx: true`.
- * If you want to use other JSX library (in typescript or javascript),
- * pass an object to the `options.jsx` for further customization, because
- * it is directly passed to `preset-react` with its defaults.
+ * If you want to use JSX (React) pass `options.jsx: true`.
+ * If you want to use JSX (React) + TypeScript pass both `{ jsx: true, typescript: true }`.
+ * If you wan to use Preact + TypeScript, `{ jsx: { pragma: 'h' }, typescript: true }`,
+ * if `options.jsx` is an object, it is directly passed to `preset-react`.
  *
  *
  * @name  babelPresetOptimize
@@ -15,6 +15,7 @@
  * @param {boolean} options.jsx - default `false`, pass `true` if you want `react`; pass an object for more customization (passed to react preset)
  * @param {boolean} options.commonjs - default `false`, pass non-falsey value to transform ESModules to CommonJS
  * @param {boolean} options.typescript - default `false`, includes the TypeScript preset
+ * @param {boolean} options.development - default `false`, disables few plugins; when it is `true` and `options.jsx` is enabled (true or object) we add `options.jsx.development: true` too
  * @param {boolean} options.minifyBuiltins - default `false`, includes [babel-plugin-minify-builtins][]
  * @public
  */
@@ -22,22 +23,33 @@ module.exports = function babelPresetOptimize(api, options) {
   api.assertVersion(7);
 
   // NOTE: minifyBuiltins: true might output a bigger output - it depends, try your codebase.
-  const {
-    jsx = false,
-    commonjs = false,
-    typescript = false,
-    minifyBuiltins = false,
-  } = { ...options };
+  const opts = {
+    jsx: false,
+    commonjs: false,
+    typescript: false,
+    development: false,
+    minifyBuiltins: false,
+    ...options,
+  };
 
-  const hasJsxOptions = jsx && typeof jsx === 'object';
-  const pragma = hasJsxOptions ? jsx.pragma : undefined;
-  const jsxPreset = hasJsxOptions ? ['@babel/preset-react', jsx] : undefined;
+  if (opts.development === true && opts.jsx) {
+    opts.jsx =
+      opts.jsx === true
+        ? { development: true }
+        : { ...opts.jsx, development: true };
+  }
 
-  const react = jsx === true ? '@babel/preset-react' : jsxPreset;
+  const hasJsxOptions = opts.jsx && typeof opts.jsx === 'object';
+  const pragma = hasJsxOptions ? opts.jsx.pragma : undefined;
+  const jsxPreset = hasJsxOptions
+    ? ['@babel/preset-react', opts.jsx]
+    : undefined;
+
+  const react = opts.jsx === true ? '@babel/preset-react' : jsxPreset;
 
   return {
     presets: [
-      typescript && [
+      opts.typescript && [
         '@babel/preset-typescript',
         { jsxPragma: pragma, isTSX: true, allExtensions: true },
       ],
@@ -45,10 +57,10 @@ module.exports = function babelPresetOptimize(api, options) {
       react,
     ].filter(Boolean),
     plugins: [
-      commonjs && '@babel/plugin-transform-modules-commonjs',
+      opts.commonjs && '@babel/plugin-transform-modules-commonjs',
       'babel-plugin-annotate-pure-calls',
       'babel-plugin-dev-expression',
-      minifyBuiltins && 'babel-plugin-minify-builtins',
+      opts.minifyBuiltins && 'babel-plugin-minify-builtins',
       'babel-plugin-minify-constant-folding',
       'babel-plugin-transform-node-env-inline',
       'babel-plugin-transform-inline-environment-variables',
@@ -57,7 +69,7 @@ module.exports = function babelPresetOptimize(api, options) {
       ['babel-plugin-transform-remove-console', { exclude: ['error', 'warn'] }],
       'babel-plugin-transform-remove-undefined',
       'babel-plugin-transform-undefined-to-void',
-      'babel-plugin-unassert',
+      opts.development !== true && 'babel-plugin-unassert',
 
       // ! enable when fix https://github.com/babel/minify/issues/973
       // ! use `terser` instead! through rollup for example.
@@ -66,10 +78,11 @@ module.exports = function babelPresetOptimize(api, options) {
 
       react && 'babel-plugin-transform-react-constant-elements',
       react && 'babel-plugin-transform-react-pure-class-to-function',
-      react && [
-        'babel-plugin-transform-react-remove-prop-types',
-        { removeImport: true },
-      ],
+      opts.development !== true &&
+        react && [
+          'babel-plugin-transform-react-remove-prop-types',
+          { removeImport: true },
+        ],
     ].filter(Boolean),
   };
 };
