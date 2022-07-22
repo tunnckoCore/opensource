b547189fba552a8c50ea304edfe00ff68f27258c
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2022-07-22 19:27:44 +0300

    chore(release): `xaxa@v4.1.0`, upgrade it to latest `yaro`
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

--- xaxa/xaxa/src/bin.js
+++ xaxa/xaxa/src/bin.js
@@ -2,21 +2,50 @@
 
 // SPDX-License-Identifier: Apache-2.0
 
+import process from 'node:process';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-
-import { readJSON, command } from './index.js';
+import { createCli, command, isRequired } from 'yaro';
+import { readJSON, lint } from './index.js';
 
 const pkgRoot = path.dirname(path.dirname(fileURLToPath(import.meta.url)));
 const pkg = readJSON(path.join(pkgRoot, 'package.json'));
+const cwd = process.cwd();
 
+const xaxa = command(
+  '[...files]',
+  'Lint and format files with ESLint --fix and Prettier.',
+)
+  .option('--cwd', 'Working directory, defaults to `process.cwd()`.', cwd)
+  .option('--log', 'Log per changed file', false)
+  .option('-f, --force', 'Force lint, cleaning the cache.', false)
+  .option('-c, --config', 'Path to config file.', {
+    default: 'xaxa.config.js',
+    required: isRequired,
+  })
+  .option('--workspace-file', 'File path to write workspaces metadata.', {
+    default: 'hela-workspace.json',
+    required: isRequired,
+    // type: 'string',
+    // normalize: true,
+  })
+  .option('--verbose', 'Print more verbose output.', false)
+  .action(async (options, files) => {
+    await lint(files, options);
+  });
+
+await createCli({
+  commands: { xaxa },
+  version: pkg.version,
+  name: pkg.name,
+});
 // you can pass a program (hela) instance as second argument,
 // otherwise it automatically creates a program in single command mode
-const prog = await command({
-  helpByDefault: false, // we want to run with the default globs patterns
-  allowUnknownOptions: true,
-  cliVersion: pkg.version,
-});
+// const prog = await command({
+//   helpByDefault: false, // we want to run with the default globs patterns
+//   allowUnknownOptions: true,
+//   cliVersion: pkg.version,
+// });
 
-// await not needed, but for safety
-await prog.parse();
+// // await not needed, but for safety
+// await prog.parse();
